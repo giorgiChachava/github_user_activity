c61e52b6c91bd2d3c70ef810926bb66618147ea4
giorgichachava <giorgi.chachava456@gmail.com>
2026-07-30 22:08:14 +0400

    added requests and changed readme a bit

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ import sys
 import json
 import time
 from pathlib import Path
-
+import requests
 from .visuals import typeout, typeout2, typeout3
 
 
 def show_greetings():
-    print()
     typeout2("---------------------------")
     typeout3("""   /$$$$$$  /$$   /$$  /$$$$$$ 
  /$$__  $$| $$  | $$ /$$__  $$
@@ -22,18 +21,31 @@ def show_greetings():
     time.sleep(0.3)
     typeout("----- Hello, I am here to help you search for peoples actvities on github")
     typeout("----- You can call me GUA.")
-    typeout("----- To use me write following instructons: github-activity <username>")
+    typeout("----- To use me, write following instructons: github-activity <username>")
     typeout("----- Now we can stalk people even without openning our browser. HOOORAYYY")
 
+def show_events(response):
+    data = response.json()
+    print(data)
+    
 def main():
     args=sys.argv[1:]
     if len(args)==0:
         show_greetings()
-    if len(args!=1):
+    elif len(args)!=1:
         typeout("----- Nuhuh. Github usernames consists of only 1 string.")
-        typeout("----- To use me write following instructons: github-activity <username>")
+        typeout("----- To use me, write following instructons: github-activity <username>")
+    else:
+        username=args[0]
+        url = f"https://api.github.com/users/{username}/events/public"
+        response = requests.get(url)
         
-    
+        if response.status_code == 200:
+            show_events(response)
+        elif response.status_code == 404:
+            print("----- Username isn't registered on GitTub")
+        else:
+            print("----- Sorry there is an error and I am unable to help you...")
     
 
     
--- app/visuals.py
+++ app/visuals.py
@@ -2,18 +2,30 @@ import sys
 import time
 
 def typeout(text, delay=0.02):
+    if not sys.stdout.isatty():
+        print(text)
+        return
+
     for char in text:
         print(char, end="", flush=True)
         time.sleep(delay)
     print()
 
 def typeout2(text, delay=0.004):
+    if not sys.stdout.isatty():
+        print(text)
+        return
+
     for char in text:
         print(char, end="", flush=True)
         time.sleep(delay)
     print()
     
-def typeout3(text, delay=0.0003):
+def typeout3(text, delay=0.0005):
+    if not sys.stdout.isatty():
+        print(text)
+        return
+
     for char in text:
         print(char, end="", flush=True)
         time.sleep(delay)
